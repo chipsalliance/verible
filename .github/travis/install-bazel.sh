fa92a41dfe0783a50c85702c0c12a0ebe8f9c4e2
Tim 'mithro' Ansell <me@mith.ro>
2019-12-02 15:25:45 -0800

    travis: Deploy to github pages and releases.
     * Deploy markdown files (including generated lint rules) to GitHub
       pages (IE the gh-pages branch).
    
     * Deploy the precompiled binaries to GitHub releases.
    
    Signed-off-by: Tim 'mithro' Ansell <me@mith.ro>

--- .github/travis/install-bazel.sh
+++ .github/travis/install-bazel.sh
@@ -13,14 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-find -L -name \*.log
-for F in $(find -L -name \*.log); do
-    echo
-    export FOLD_NAME=$(echo $F | sed -e's/[^A-Za-z0-9]/_/g')
-    travis_fold start $FOLD_NAME
-    echo -e "\n\n$F\n--------------"
-    cat $F
-    echo "--------------"
-    travis_fold end $FOLD_NAME
-    echo
-done
+wget https://github.com/bazelbuild/bazel/releases/download/1.2.0/bazel_1.2.0-linux-x86_64.deb -O /tmp/bazel.deb
+sudo dpkg -i /tmp/bazel.deb || true
+sudo apt-get -f install
+bazel --version
--- .github/travis/set-compiler.sh
+++ .github/travis/set-compiler.sh
@@ -13,14 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-find -L -name \*.log
-for F in $(find -L -name \*.log); do
-    echo
-    export FOLD_NAME=$(echo $F | sed -e's/[^A-Za-z0-9]/_/g')
-    travis_fold start $FOLD_NAME
-    echo -e "\n\n$F\n--------------"
-    cat $F
-    echo "--------------"
-    travis_fold end $FOLD_NAME
-    echo
-done
+VERSION=$1
+
+sudo dpkg --list | grep gcc
+sudo ln -sf /usr/bin/gcc-$VERSION /usr/bin/gcc
+sudo ln -sf /usr/bin/g++-$VERSION /usr/bin/g++
+gcc --version || true
