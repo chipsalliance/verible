1182b9b517652e0289bf6e38be4dc525be66cc52
David Fang <fangism@google.com>
2020-01-02 10:01:53 -0800

    Happy New Year, 2020.
    Update copyright dates.
    
    PiperOrigin-RevId: 287851492

--- common/parser/move_yacc_stack_symbols.sh
+++ common/parser/move_yacc_stack_symbols.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2017-2019 The Verible Authors.
+# Copyright 2017-2020 The Verible Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
