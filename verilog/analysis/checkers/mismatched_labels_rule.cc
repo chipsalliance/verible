890c9760f57864ed7cd7c6c533cdc944eb74ac5e
Henner Zeller <hzeller@google.com>
2020-01-16 14:26:30 -0800

    PR #88: Find label mismatches between begin/end pairs Fixes: #85
    GitHub PR https://github.com/google/verible/pull/88
    
    Copybara import of the project:
    
      - a99f52fbff81a21e277e866170ba711f5c1b6ea8 Add seq_block CST utils by Tomasz Gorochowik <tgorochowik@antmicro.com>
      - bfc5c6344eb07f059ee7965ad0385f0959578d94 Add mismatched labels linting rule by Tomasz Gorochowik <tgorochowik@antmicro.com>
      - 3a86ec6b956844e7c027939b8a36c7e2042be400 Add mismatched labels integration test by Tomasz Gorochowik <tgorochowik@antmicro.com>
      - 9a3164c708a4b3b965c086db6ed8611653994d23 Add mismatched labels lint rule unit tests by Tomasz Gorochowik <tgorochowik@antmicro.com>
    
    Closes #88
    
    PiperOrigin-RevId: 290145585

---
+++ verilog/analysis/checkers/mismatched_labels_rule.cc
@@ -0,0 +1,94 @@
+// Copyright 2017-2020 The Verible Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "verilog/analysis/checkers/mismatched_labels_rule.h"
+
+#include <set>
+#include <string>
+#include <vector>
+
+#include "absl/strings/str_cat.h"
+#include "common/analysis/citation.h"
+#include "common/analysis/lint_rule_status.h"
+#include "common/analysis/matcher/bound_symbol_manager.h"
+#include "common/analysis/matcher/matcher.h"
+#include "common/analysis/syntax_tree_search.h"
+#include "common/text/concrete_syntax_tree.h"
+#include "common/text/symbol.h"
+#include "common/text/syntax_tree_context.h"
+#include "common/text/tree_utils.h"
+#include "verilog/CST/identifier.h"
+#include "verilog/CST/seq_block.h"
+#include "verilog/analysis/descriptions.h"
+#include "verilog/analysis/lint_rule_registry.h"
+
+namespace verilog {
+namespace analysis {
+
+using verible::GetStyleGuideCitation;
+
+// Register the lint rule
+VERILOG_REGISTER_LINT_RULE(MismatchedLabelsRule);
+
+absl::string_view MismatchedLabelsRule::Name() { return "mismatched-labels"; }
+const char MismatchedLabelsRule::kTopic[] = "mismatched-labels";
+const char MismatchedLabelsRule::kMessageMismatch[] =
+    "Begin/end block labels must match.";
+const char MismatchedLabelsRule::kMessageMissing[] =
+    "Matching begin label is missing.";
+
+std::string MismatchedLabelsRule::GetDescription(
+    DescriptionType description_type) {
+  return absl::StrCat("Labels mismatch. See:", GetStyleGuideCitation(kTopic),
+                      ".");
+}
+
+void MismatchedLabelsRule::HandleSymbol(
+    const verible::Symbol& symbol, const verible::SyntaxTreeContext& context) {
+  verible::matcher::BoundSymbolManager manager;
+
+  if (matcher_.Matches(symbol, &manager)) {
+    const auto& matchingEnd = GetMatchingEnd(symbol, context);
+
+    const auto* begin_label = GetBeginLabelTokenInfo(symbol);
+    const auto* end_label = GetEndLabelTokenInfo(*matchingEnd);
+
+    // Don't check anything if there is no end label
+    if (end_label == nullptr) {
+      return;
+    }
+
+    // Error if there is no begin label
+    if (begin_label == nullptr) {
+      violations_.insert(
+          verible::LintViolation(symbol, kMessageMissing, context));
+
+      return;
+    }
+
+    // Finally compare the two labels
+    if (begin_label->text != end_label->text) {
+      violations_.insert(
+          verible::LintViolation(*end_label, kMessageMismatch, context));
+    }
+  }
+}
+
+verible::LintRuleStatus MismatchedLabelsRule::Report() const {
+  return verible::LintRuleStatus(violations_, Name(),
+                                 GetStyleGuideCitation(kTopic));
+}
+
+}  // namespace analysis
+}  // namespace verilog
