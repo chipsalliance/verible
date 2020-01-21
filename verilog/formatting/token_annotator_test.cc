dbb68e1a1772c9e828a36f49554c5f49334c8fe1
Henner Zeller <hzeller@google.com>
2020-01-21 13:34:50 -0800

    PR #138: Begin keyword must be on the same line as preceeding ')' token Fixes #134
    GitHub PR https://github.com/google/verible/pull/138
    
    Copybara import of the project:
    
      - 16b32b7c2a4c5de034645f8fc064cfdd82b8f9a4 Begin keyword must be on the same line as preceeding ')' ... by Lukasz Dalek <ldalek@antmicro.com>
    
    Closes #138
    
    PiperOrigin-RevId: 290810623

--- verilog/formatting/token_annotator_test.cc
+++ verilog/formatting/token_annotator_test.cc
@@ -1327,15 +1327,15 @@ TEST(TokenAnnotatorTest, AnnotateFormattingInfoTest) {
                {1, SpacingOptions::Undecided},  // t
                {0, SpacingOptions::Undecided},  // )
 
-               {1, SpacingOptions::Preserve},   // begin
-               {1, SpacingOptions::Undecided},  // a
-               {0, SpacingOptions::Undecided},  // .
-               {0, SpacingOptions::Undecided},  // b
-               {0, SpacingOptions::Undecided},  // (
-               {0, SpacingOptions::Undecided},  // c
-               {0, SpacingOptions::Undecided},  // )
-               {0, SpacingOptions::Undecided},  // ;
-               {1, SpacingOptions::MustWrap},   // end
+               {1, SpacingOptions::MustAppend},  // begin
+               {1, SpacingOptions::Undecided},   // a
+               {0, SpacingOptions::Undecided},   // .
+               {0, SpacingOptions::Undecided},   // b
+               {0, SpacingOptions::Undecided},   // (
+               {0, SpacingOptions::Undecided},   // c
+               {0, SpacingOptions::Undecided},   // )
+               {0, SpacingOptions::Undecided},   // ;
+               {1, SpacingOptions::MustWrap},    // end
 
                {1, SpacingOptions::MustAppend},  // else
 
