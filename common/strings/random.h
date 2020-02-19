9befbbf106465b1799c8a81144bc076b9780e2fb
David Fang <fangism@google.com>
2020-02-19 13:02:27 -0800

    Random identifier generator.
    Initial version matches length of input string.
    
    PiperOrigin-RevId: 296037221

---
+++ common/strings/random.h
@@ -0,0 +1,35 @@
+// Copyright 2017-2020 The Verible Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef VERIBLE_COMMON_STRINGS_RANDOM_H_
+#define VERIBLE_COMMON_STRINGS_RANDOM_H_
+
+#include <string>
+
+#include "absl/strings/string_view.h"
+
+namespace verible {
+
+// Returns a random character in [a-zA-Z].
+char RandomAlphaChar();
+
+// Returns a random character in [a-zA-Z0-9].
+char RandomAlphaNumChar();
+
+// Returns an identifier ([alpha][alnum]*) of equal length to input.
+std::string RandomEqualLengthIdentifier(absl::string_view input);
+
+}  // namespace verible
+
+#endif  // VERIBLE_COMMON_STRINGS_RANDOM_H_
