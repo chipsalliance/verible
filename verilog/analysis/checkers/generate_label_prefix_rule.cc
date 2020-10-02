7e9d83e8d046feecfe0bee4a9524fe5e141a0003
Tomasz Gorochowik <t@gorochowik.com>
2020-10-02 09:07:44 -0700

    PR #468: Generate label prefix rule Fixes #311
    GitHub PR https://github.com/google/verible/pull/468
    
    Copybara import of the project:
    
      - a32125a67c73b7a144287bf1668ad9e84ff8b2d3 Update the generate_begin test not to violate the upcomin... by Tomasz Gorochowik <tgorochowik@antmicro.com>
      - 14125e7c0521a0411737e5126158b21b7b3c6d5c Add generate label prefix rule by Tomasz Gorochowik <tgorochowik@antmicro.com>
      - 03011d24c8f26a8d673b9b22862c559ca24f046d Add integration test for label prefix in generate blocks by Tomasz Gorochowik <tgorochowik@antmicro.com>
      - a6edd7c66c146ba2aff8a831ff68141733e8d522 Merge 03011d24c8f26a8d673b9b22862c559ca24f046d into 1c187... by Tomasz Gorochowik <t@gorochowik.com>
    
    Closes #468
    
    PiperOrigin-RevId: 335037391

---
+++ verilog/analysis/checkers/generate_label_prefix_rule.cc
@@ -0,0 +1,94 @@
+// Copyright 2017-2020 The Verible Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "verilog/analysis/checkers/generate_label_prefix_rule.h"
+
+#include <string>
+#include <vector>
+
+#include "absl/strings/match.h"
+#include "absl/strings/str_cat.h"
+#include "common/analysis/citation.h"
+#include "common/analysis/lint_rule_status.h"
+#include "common/analysis/matcher/bound_symbol_manager.h"
+#include "common/analysis/matcher/matcher.h"
+#include "common/text/symbol.h"
+#include "common/text/syntax_tree_context.h"
+#include "verilog/CST/identifier.h"
+#include "verilog/CST/seq_block.h"
+#include "verilog/analysis/descriptions.h"
+#include "verilog/analysis/lint_rule_registry.h"
+
+namespace verilog {
+namespace analysis {
+
+using verible::GetStyleGuideCitation;
+
+// Register the lint rule
+VERILOG_REGISTER_LINT_RULE(GenerateLabelPrefixRule);
+
+absl::string_view GenerateLabelPrefixRule::Name() {
+  return "generate-label-prefix";
+}
+const char GenerateLabelPrefixRule::kTopic[] = "generate-statements";
+const char GenerateLabelPrefixRule::kMessage[] =
+    "All generate block labels must start with g_ or gen_";
+
+std::string GenerateLabelPrefixRule::GetDescription(
+    DescriptionType description_type) {
+  return absl::StrCat(
+      "Checks that every generate block label starts with g_ or gen_. See ",
+      GetStyleGuideCitation(kTopic), ".");
+}
+
+void GenerateLabelPrefixRule::HandleSymbol(
+    const verible::Symbol& symbol, const verible::SyntaxTreeContext& context) {
+  verible::matcher::BoundSymbolManager manager;
+  if (matcher_.Matches(symbol, &manager)) {
+    // Exclude case generate statements, as kGenerateBlock is generated for
+    // each 'case' item too.
+    if (context.IsInside(NodeEnum::kGenerateCaseItemList)) {
+      return;
+    }
+
+    for (const auto& child : SymbolCastToNode(symbol).children()) {
+      const verible::TokenInfo* label = nullptr;
+      switch (NodeEnum(SymbolCastToNode(*child).Tag().tag)) {
+        case NodeEnum::kBegin:
+          label = GetBeginLabelTokenInfo(*child);
+          break;
+        case NodeEnum::kEnd:
+          label = GetEndLabelTokenInfo(*child);
+          break;
+        default:
+          continue;
+      }
+
+      if (label != nullptr) {
+        if (!(absl::StartsWith(label->text(), "g_") ||
+              absl::StartsWith(label->text(), "gen_"))) {
+          violations_.insert(verible::LintViolation(*label, kMessage, context));
+        }
+      }
+    }
+  }
+}
+
+verible::LintRuleStatus GenerateLabelPrefixRule::Report() const {
+  return verible::LintRuleStatus(violations_, Name(),
+                                 GetStyleGuideCitation(kTopic));
+}
+
+}  // namespace analysis
+}  // namespace verilog
