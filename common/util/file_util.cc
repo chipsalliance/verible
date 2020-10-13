bb6540469dfd55628e8bf7a9aaaabeb4f5bae434
Googler <noreply@google.com>
2020-10-12 18:29:45 -0700

    Internal change
    PiperOrigin-RevId: 336787230

--- common/util/file_util.cc
+++ common/util/file_util.cc
@@ -29,6 +29,7 @@
 #include <string>
 
 #include "absl/status/status.h"
+#include "absl/status/statusor.h"
 #include "absl/strings/str_cat.h"
 #include "absl/strings/str_join.h"
 #include "absl/strings/str_split.h"
