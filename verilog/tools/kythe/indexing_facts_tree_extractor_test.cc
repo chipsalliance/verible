5b56693015cfdc86de48fcaaa0d70305adf67951
MinaToma <minatoma1999@gmail.com>
2020-10-14 10:22:00 +0000

    Tests

--- verilog/tools/kythe/indexing_facts_tree_extractor_test.cc
+++ verilog/tools/kythe/indexing_facts_tree_extractor_test.cc
@@ -14,10 +14,10 @@
 
 #include "verilog/tools/kythe/indexing_facts_tree_extractor.h"
 
-#include "gtest/gtest.h"
 #include "common/analysis/syntax_tree_search_test_utils.h"
 #include "common/text/concrete_syntax_tree.h"
 #include "common/util/file_util.h"
+#include "gtest/gtest.h"
 #include "verilog/analysis/verilog_analyzer.h"
 
 #undef EXPECT_OK
@@ -2105,6 +2105,77 @@ TEST(FactsTreeExtractor, FunctionAndTaskDeclarationWithArgs) {
                       Anchor(kTestCase.expected_tokens[21], kTestCase.code),
                   },
                   IndexingFactType ::kVariableDefinition,
+              })))));
+
+  const auto facts_tree =
+      ExtractFiles({std::string(verible::file::Basename(test_file.filename()))},
+                   exit_status, testing::TempDir());
+
+  const auto result_pair = DeepEqual(facts_tree, expected);
+  EXPECT_EQ(result_pair.left, nullptr) << *result_pair.left;
+  EXPECT_EQ(result_pair.right, nullptr) << *result_pair.right;
+}
+
+TEST(FactsTreeExtractor, ClassMember) {
+  constexpr int kTag = 1;  // value doesn't matter
+  const verible::SyntaxTreeSearchTestCase kTestCase = {{
+      "module ",
+      {kTag, "m"},
+      "();\ninitial begin\n$display(",
+      {kTag, "my_class"},
+      ".",
+      {kTag, "x"},
+      ");\n$display(",
+      {kTag, "my_class"},
+      ".",
+      {kTag, "instance1"},
+      ".",
+      {kTag, "y"},
+      ");\nend\nendmodule",
+  }};
+
+  ScopedTestFile test_file(testing::TempDir(), kTestCase.code);
+  int exit_status = 0;
+
+  const IndexingFactNode expected(T(
+      {
+          {
+              Anchor(testing::TempDir(), 0, 0),
+          },
+          IndexingFactType::kFileList,
+      },
+      T(
+          {
+              {
+                  Anchor(test_file.filename(), 0, kTestCase.code.size()),
+                  Anchor(kTestCase.code, 0, kTestCase.code.size()),
+              },
+              IndexingFactType ::kFile,
+          },
+          // refers to module m
+          T(
+              {
+                  {
+                      Anchor(kTestCase.expected_tokens[1], kTestCase.code),
+                  },
+                  IndexingFactType::kModule,
+              },
+              // refers to my_class.x
+              T({
+                  {
+                      Anchor(kTestCase.expected_tokens[3], kTestCase.code),
+                      Anchor(kTestCase.expected_tokens[5], kTestCase.code),
+                  },
+                  IndexingFactType ::kMemberReference,
+              }),
+              // refers to my_class.x
+              T({
+                  {
+                      Anchor(kTestCase.expected_tokens[7], kTestCase.code),
+                      Anchor(kTestCase.expected_tokens[9], kTestCase.code),
+                      Anchor(kTestCase.expected_tokens[11], kTestCase.code),
+                  },
+                  IndexingFactType ::kMemberReference,
               })))));
 
   const auto facts_tree =
