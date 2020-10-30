17b47252755bddaf783270bfa12216d591d0ade1
MinaToma <minatoma1999@gmail.com>
2020-10-30 14:49:36 +0000

    Remove Log

--- verilog/tools/kythe/verilog_kythe_extractor.cc
+++ verilog/tools/kythe/verilog_kythe_extractor.cc
@@ -154,7 +154,6 @@ static std::vector<absl::Status> ExtractFiles(
   if (absl::GetFlag(FLAGS_printextraction)) {
     std::cout << file_list_facts_tree << std::endl;
   }
-  LOG(INFO) << file_list_facts_tree << std::endl;
 
   // check how kythe facts.
   switch (absl::GetFlag(FLAGS_print_kythe_facts)) {
