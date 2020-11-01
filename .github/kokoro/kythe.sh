4619756a9faf95c48b567616b25b2b67ecbea27f
David Fang <fangism@google.com>
2020-10-31 22:24:34 -0700

    Update kokoro's kythe script.
    Remove sudo.
    
    Only export needed environment variables, most are local.
    
    Check script prerequites, show bazel version.
    
    More comments.
    
    PiperOrigin-RevId: 340076740

--- .github/kokoro/kythe.sh
+++ .github/kokoro/kythe.sh
@@ -13,6 +13,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# This script indexes Verible's source code using kythe and its C++ indexer.
+# This can be run as a continuous integration step to produce "kzip" files
+# on every commit.
+
 set -x
 set -e
 
@@ -21,6 +25,14 @@ source "${KOKORO_GFILE_DIR}/common_google.sh"
 
 # Cleanup previous build cache.
 rm -rf "$HOME/.cache/bazel"
+
+# Rely on set -e to fail on first missing prereq.
+echo "Checking prerequisites ..."
+which wget
+which unzip
+which tar
+which bazel && bazel version
+echo "... done."
 
 # Setup kythe
 # TODO(ikr): bump to version >= 0.0.49 after it is released
@@ -31,30 +43,40 @@ cd "${TMPDIR}"
 
 if [[ "$KYTHE_VERSION" == "master" ]]
 then
+  # Note: top-of-tree archive does not come with binaries
+  # Caution: Building kythe itself will require all of its build dependencies
+  # and be extremely compute-intensive.
   wget -q -O kythe.zip \
     https://github.com/kythe/kythe/archive/master.zip
   unzip kythe.zip
 else
-  # use release
+  # Use release, which comes with pre-built binaries
   wget -q -O kythe.tar.gz \
     "https://github.com/kythe/kythe/releases/download/$KYTHE_VERSION/kythe-$KYTHE_VERSION.tar.gz"
   tar -xzf kythe.tar.gz
 fi
 
 KYTHE_DIRNAME="kythe-${KYTHE_VERSION}"
-sudo cp -R "${KYTHE_DIRNAME}" /opt
-KYTHE_DIR="/opt/${KYTHE_DIRNAME}"
-sudo chmod -R 755 "${KYTHE_DIR}"
-echo "KYTHE_DIR=${KYTHE_DIR}" >> ~/.bashrc
+KYTHE_DIR_ABS="$(readlink -f "${KYTHE_DIRNAME}")"
+
+# TODO(fangism): KYTHE_DIR is not referenced outside of this script.
+#   Remove the following?  Remove reliance on .bashrc variables where possible.
+if grep -q "KYTHE_DIR=" ~/.bashrc
+then
+  sed -i -e "/KYTHE_DIR=/s|=.*|=${KYTHE_DIR_ABS}|" ~/.bashrc
+else
+  echo "KYTHE_DIR=${KYTHE_DIR_ABS}" >> ~/.bashrc
+fi
 
 # Prepare the environment for verible build
-cd "${KOKORO_ARTIFACTS_DIR}/github/verible/"
+cd "${KOKORO_ARTIFACTS_DIR}/github/verible"
 
 # Downloads bazelisk to ~/bin as `bazel`.
 function set_bazel_outdir {
   mkdir -p /tmpfs/bazel_output
   export TEST_TMPDIR=/tmpfs/bazel_output
 }
+
 function install_bazelisk {
   date
   case "$(uname -s)" in
@@ -70,24 +92,23 @@ function install_bazelisk {
     PATH="$HOME/bin:$PATH"
   fi
   set_bazel_outdir
-  which bazel
-  bazel version
   date
 }
 
 # Create the kythe output directory
-export KYTHE_OUTPUT_DIRECTORY="${KOKORO_ARTIFACTS_DIR}/kythe_output"
+KYTHE_OUTPUT_DIRECTORY="${KOKORO_ARTIFACTS_DIR}/kythe_output"
 mkdir -p "${KYTHE_OUTPUT_DIRECTORY}"
 
-# Build everything
-# kythe_release expects an absolute dir
-bazel --bazelrc="${KYTHE_DIR}/extractors.bazelrc" \
-  build --override_repository kythe_release="${KYTHE_DIR}" \
+# Build everything in Verible to index its source
+# --override_repository kythe_release expects an absolute dir
+bazel --bazelrc="${KYTHE_DIR_ABS}/extractors.bazelrc" \
+  build --override_repository kythe_release="${KYTHE_DIR_ABS}" \
   --define=kythe_corpus=github.com/google/verible \
   -- \
   //...
 
 # Merge the kzips and move them to kokoro artifacts directory.
-"$KYTHE_DIR/tools/kzip" merge \
+# Note: kzip tool path assumes it came with the release pre-built.
+"$KYTHE_DIR_ABS/tools/kzip" merge \
   --output "${KYTHE_OUTPUT_DIRECTORY}/${KOKORO_GIT_COMMIT}_${KOKORO_BUILD_NUMBER}.kzip" \
   $(find bazel-out/*/extra_actions/ -name "*.kzip")
