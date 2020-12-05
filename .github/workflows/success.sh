8aaeca17158b0d8f13b77a4667ee51214ff5edb3
Tim 'mithro' Ansell <me@mith.ro>
2020-12-04 17:53:02 -0800

    gha: Export and then use tag value.
    Signed-off-by: Tim 'mithro' Ansell <me@mith.ro>

--- .github/workflows/success.sh
+++ .github/workflows/success.sh
@@ -34,6 +34,7 @@ bin)
     # Create a tag of form v0.0-183-gdf2b162-20191112132344
     rm -rf releasing/out
     git tag "$TAG" || true
+    echo "::set-output name=TAG::$(echo $TAG)"
     ls -l /tmp/releases
     ;;
 
