dc6e959af23aaffd6741a452a8c28f0669975ea4
umarcor <38422348+umarcor@users.noreply.github.com>
2020-12-08 17:26:26 -0500

    PR #623: ci: generate Release matrix dynamically Close #621.
    As suggested by @mithro and explained by @tgorochowik, this PR generates the Release matrix dynamically.
    
    GitHub PR https://github.com/google/verible/pull/623
    
    Copybara import of the project:
    
      - 05b9c0be17ddb0b65ffdff2d75e8f2b2b45008a0 ci: generate Release matrix dynamically by umarcor <unai.martinezcorral@ehu.eus>
      - 6b61ec994ad0856656698ae802ec68b622a11a9c releasing: disable centos-6 by umarcor <unai.martinezcorral@ehu.eus>
      - 8b16f4ccff03c6637c0de166b4b2bbc11c024354 ci: add script 'generate_matrix.sh' by umarcor <unai.martinezcorral@ehu.eus>
      - 7feedc3bb7d0b828dbca7b956a41544bbe1c3db9 Merge 8b16f4ccff03c6637c0de166b4b2bbc11c024354 into 3a55e... by umarcor <38422348+umarcor@users.noreply.github.com>
    
    Closes #623
    
    PiperOrigin-RevId: 346410775

---
+++ .github/workflows/generate_matrix.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# Copyright 2020 The Verible Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+matrix='['
+for item in releasing/*/; do
+  matrix+='{"OS": "'"`echo $(basename $item) | cut -d- -f1`"'", "OS_VERSION": "'"`echo $(basename $item) | cut -d- -f2`"'"},'
+done
+matrix+=']'
+
+echo "$matrix"
+
+echo "::set-output name=matrix::$matrix"
