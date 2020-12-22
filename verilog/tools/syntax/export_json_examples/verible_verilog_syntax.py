094b90f716f04130604d37cbaab8afddcfe399c1
Mariusz Glebocki <mglebocki@antmicro.com>
2020-12-22 10:12:09 +0100

    Fix cases where token.start == 0

--- verilog/tools/syntax/export_json_examples/verible_verilog_syntax.py
+++ verilog/tools/syntax/export_json_examples/verible_verilog_syntax.py
@@ -112,7 +112,8 @@ class Node(anytree.NodeMixin):
     start = self.start
     end = self.end
     sd = self.syntax_data
-    if start and end and sd and sd.source_code and end <= len(sd.source_code):
+    if ((start is not None) and (end is not None) and sd and sd.source_code
+        and end <= len(sd.source_code)):
       return sd.source_code[start:end].decode("utf-8")
     return ""
 
