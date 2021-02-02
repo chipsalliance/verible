ed5643e44aeefec25ec386d812b8127059ee3ef1
Henner Zeller <h.zeller@acm.org>
2021-02-02 13:22:39 -0800

    Add a clang-format format check in github pull request actions.
    Issues #653
    
    Signed-off-by: Henner Zeller <h.zeller@acm.org>

---
+++ .github/workflows/run-clang-format.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Copyright 2020 The Verible Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+FORMAT_OUT=${TMPDIR:-/tmp}/clang-format-diff.out
+
+# TODO(hzeller): only run on the files that are checked out
+clang-format -i --style=Google $(find . -name "*.h" -o -name "*.cc")
+
+# Check if we got any diff
+git diff > ${FORMAT_OUT}
+
+if [ -s ${FORMAT_OUT} ]; then
+   echo "Style not matching (see https://github.com/google/verible/blob/master/CONTRIBUTING.md#style)"
+   echo "On your *.h, *.cc files, please run clang-format -i --style=Google"
+   cat ${FORMAT_OUT}
+   exit 1
+fi
+
+exit 0
