ff0605a44f7b1e2dce16aefcabcd4a4dc4ef41c1
Henner Zeller <h.zeller@acm.org>
2021-03-23 18:05:51 -0700

    make more verbose.

--- .github/workflows/run-clang-format.sh
+++ .github/workflows/run-clang-format.sh
@@ -12,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+set -x
 
 FILE_LIST_OUT=${TMPDIR:-/tmp}/file-list.txt
 FORMAT_OUT=${TMPDIR:-/tmp}/clang-format-diff.out
