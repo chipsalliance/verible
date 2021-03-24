b3f7f58a77df382b5e9aa840e9c0fe54c5afe32c
Henner Zeller <h.zeller@acm.org>
2021-03-23 17:52:37 -0700

    Update clang format script to be a bit more verbose.
    Signed-off-by: Henner Zeller <h.zeller@acm.org>

--- .github/workflows/run-clang-format.sh
+++ .github/workflows/run-clang-format.sh
@@ -13,18 +13,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+FILE_LIST_OUT=${TMPDIR:-/tmp}/file-list.txt
 FORMAT_OUT=${TMPDIR:-/tmp}/clang-format-diff.out
 
 # Run on all the files that are affected
-clang-format -i --style=file $(git diff --name-only --diff-filter=AM -r origin/master | grep '\(\.cc\|\.h\)$') 2> /dev/null
+
+# Finding all affected files and list them for inspection in the log output
+git diff --name-only --diff-filter=AM -r origin/master | grep '\(\.cc\|\.h\)$' | tee "${FILE_LIST_OUT}"
+
+clang-format -i --style=file $(cat "${FILE_LIST_OUT}") 2> /dev/null
 
 # Check if we got any diff
-git diff > ${FORMAT_OUT}
+git diff > "${FORMAT_OUT}"
 
-if [ -s ${FORMAT_OUT} ]; then
+if [ -s "${FORMAT_OUT}" ]; then
    echo "Style not matching (see https://github.com/google/verible/blob/master/CONTRIBUTING.md#style)"
    echo "On your *.h, *.cc files, please run clang-format -i --style=file <your changed files>"
-   cat ${FORMAT_OUT}
+   cat "${FORMAT_OUT}"
    exit 1
 fi
 
