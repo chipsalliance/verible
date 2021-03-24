b4fcf05924fa35232ccf9af2ea22361d36485d4a
Henner Zeller <h.zeller@acm.org>
2021-03-23 18:57:44 -0700

    See if file is opened..

--- .github/workflows/run-clang-format.sh
+++ .github/workflows/run-clang-format.sh
@@ -20,8 +20,6 @@ function cleanup {
 }
 trap cleanup EXIT
 
-clang-format --help
-
 # Run on all the files that are affected
 
 # Finding all affected files and list them for inspection in the log output
@@ -29,7 +27,7 @@ git diff --name-only --diff-filter=AM -r origin/master | grep '\(\.cc\|\.h\)$' |
 
 for f in $(cat "${FILE_LIST_OUT}") ; do
   cp ${f} ${f}.before-clang-format
-  clang-format --verbose -i --style=Google ${f} 2> /dev/null
+  strace -eopen clang-format --verbose -i --style=file ${f} 2> /dev/null
   diff -u ${f}.before-clang-format ${f} >> ${FORMAT_OUT}
   echo "Exit code diff $?"
 done
