de9cd4ba42f82f6059ef0a3e11c9e0e41144ed28
Henner Zeller <h.zeller@acm.org>
2021-03-23 19:18:25 -0700

    Juse use style Google instead of file.

--- .github/workflows/run-clang-format.sh
+++ .github/workflows/run-clang-format.sh
@@ -12,31 +12,19 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-FILE_LIST_OUT=${TMPDIR:-/tmp}/file-list.$$.txt
-FORMAT_OUT=${TMPDIR:-/tmp}/clang-format-diff.$$.out
 
-function cleanup {
-  rm -f ${FORMAT_OUT} ${FORMAT_LIST_OUT}
-}
-trap cleanup EXIT
+FORMAT_OUT=${TMPDIR:-/tmp}/clang-format-diff.out
 
 # Run on all the files that are affected
+clang-format -i --style=Google $(git diff --name-only --diff-filter=AM -r origin/master | grep '\(\.cc\|\.h\)$') 2> /dev/null
 
-# Finding all affected files and list them for inspection in the log output
-git diff --name-only --diff-filter=AM -r origin/master | grep '\(\.cc\|\.h\)$' | tee "${FILE_LIST_OUT}"
+# Check if we got any diff
+git diff > ${FORMAT_OUT}
 
-for f in $(cat "${FILE_LIST_OUT}") ; do
-  cp ${f} ${f}.before-clang-format
-  strace -ostrace-out -eopen clang-format --verbose -i --style=file ${f} 2> /dev/null
-  diff -u ${f}.before-clang-format ${f} >> ${FORMAT_OUT}
-  cat strace-out
-  echo "Exit code diff $?"
-done
-
-if [ -s "${FORMAT_OUT}" ]; then
+if [ -s ${FORMAT_OUT} ]; then
    echo "Style not matching (see https://github.com/google/verible/blob/master/CONTRIBUTING.md#style)"
    echo "On your *.h, *.cc files, please run clang-format -i --style=file <your changed files>"
-   cat "${FORMAT_OUT}"
+   cat ${FORMAT_OUT}
    exit 1
 fi
 
