477bf500e82f26674dcfd25f92e71ac3cc42b7c3
Henner Zeller <h.zeller@acm.org>
2021-03-29 19:20:55 -0700

    Move BAZEL_OPTS back to settings.sh
    Kythe CI and release builds needs it.
    
    Signed-off-by: Henner Zeller <h.zeller@acm.org>

--- .github/settings.sh
+++ .github/settings.sh
@@ -26,6 +26,41 @@ fi
 
 export GIT_VERSION=${GIT_VERSION:-$(git describe --match=v*)}
 
+# TODO(b/171679296): re-enable c++11 support
+#   by downgrading kythe build requirements.
+export BAZEL_CXXOPTS="-std=c++17"
+
+# Reduce the verbosity of progress output on CI
+BAZEL_OPTS="-c opt --show_progress_rate_limit=10.0"
+
+# Turn warnings to 11. And fail compliation if we encounter one.
+BAZEL_OPTS="${BAZEL_OPTS} --cxxopt=-Werror"  # Always want bail on warning
+BAZEL_OPTS="${BAZEL_OPTS} --cxxopt=-Wall --cxxopt=-Wextra"
+
+# -- now disable some of the warnings that happen, so that the compile finishes.
+
+# Status-quo of warnings happening in our code-base. These are benign.
+BAZEL_OPTS="${BAZEL_OPTS} --cxxopt=-Wno-unused-parameter"
+BAZEL_OPTS="${BAZEL_OPTS} --cxxopt=-Wno-missing-field-initializers"
+
+# Warnings in our code-base, that we might consider removing.
+BAZEL_OPTS="${BAZEL_OPTS} --cxxopt=-Wno-redundant-move"
+
+# Warnings that come from other external parts that we compile.
+# Ideally, we would separate them out to ignore only there, while we keep
+# tight warnings on for 'our' code-base.
+# TODO(hzeller): figure out if bazel allows for that; WORKSPACE file ?
+BAZEL_OPTS="${BAZEL_OPTS} --cxxopt=-Wno-implicit-fallthrough"     # gflags
+BAZEL_OPTS="${BAZEL_OPTS} --cxxopt=-Wno-cast-function-type"       # gflags
+BAZEL_OPTS="${BAZEL_OPTS} --cxxopt=-Wno-sign-compare"             # glog
+BAZEL_OPTS="${BAZEL_OPTS} --cxxopt=-Wno-stringop-truncation"      # memcache
+BAZEL_OPTS="${BAZEL_OPTS} --cxxopt=-Wno-int-in-bool-context"      # memcache
+BAZEL_OPTS="${BAZEL_OPTS} --cxxopt=-Wno-deprecated-declarations"  # jsconcpp
+BAZEL_OPTS="${BAZEL_OPTS} --cxxopt=-Wno-unused-but-set-variable"  # kythe
+BAZEL_OPTS="${BAZEL_OPTS} --cxxopt=-Wno-array-bounds"             # kythe
+
+export BAZEL_OPTS
+
 export BAZEL_VERSION=3.7.0
 # Without following environment variable set, Bazel updates itself to the
 # latest version
