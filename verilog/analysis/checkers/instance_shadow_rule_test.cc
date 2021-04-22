eb85191563bcddc7024dc8935442a6e48d3a6e76
Pawel Sagan <psagan@antmicro.com>
2021-04-22 22:50:46 +0200

    adding more tests

--- verilog/analysis/checkers/instance_shadow_rule_test.cc
+++ verilog/analysis/checkers/instance_shadow_rule_test.cc
@@ -36,13 +36,56 @@ TEST(InstanceShadowingTest, FunctionPass) {
   const std::initializer_list<LintTestCase> kInstanceShadowingTestCases = {
       {""},
       {"module foo; logic a; endmodule"},
+      {"module foo; logic a, b, c;  endmodule:foo;"},
+      {"module foo; logic a; class boo; endclass:boo; endmodule:foo;"},
+      {"module foo; logic a; begin boo; bit var1; end:boo; endmodule:foo;"},
+      {"module foo; logic a; begin boo;", "if(a) begin baz\n",
+       "$display(\"\t Value of i = %0d\",i);", "\nend:baz", "\nend:boo;",
+       "endmodule:foo;"},
+      {"module foo; logic a; begin boo;",
+       "loop: for(int i=0;i<5;i++) begin baz\n",
+       "$display(\"\t Value of i = %0d\",i);", "\nend:baz", "\nend:boo;",
+       "endmodule:foo;"},
+      {
+          "module bad_string;\n",
+          "genvar i;\n",
+          "generate\n",
+          "initial begin\n",
+          "for(int i=0;i<5;i++) $display(\"\t i = %d\",i);\n",
+          "end\n",
+          "endgenerate\n",
+          "endmodule:bad_string;\n",
+      },
   };
   RunLintTestCases<VerilogAnalyzer, InstanceShadowRule>(
       kInstanceShadowingTestCases);
 }
 
 TEST(InstanceShadowingTest, FunctionFailures) {
-  const std::initializer_list<LintTestCase> kInstanceShadowingTestCases = {};
+  constexpr int kToken = SymbolIdentifier;
+  const std::initializer_list<LintTestCase> kInstanceShadowingTestCases = {
+      {"module foo; logic ", {kToken, "foo"}, "; endmodule:foo;"},
+      {"module foo; logic a; logic ", {kToken, "a"}, "; endmodule:foo;"},
+      {"module foo; logic a; class ",
+       {kToken, "foo"},
+       "; endclass:foo; endmodule:foo;"},
+      {"module foo; logic a; class boo; logic ",
+       {kToken, "boo"},
+       "; endclass:boo; endmodule:foo;"},
+      {"module foo; logic a; class boo; logic ",
+       {kToken, "foo"},
+       "; endclass:boo; endmodule:foo;"},
+      {
+          "module foo;\n",
+          "int i;\n",
+          "initial begin\n",
+          "for(int ",
+          {kToken, "i"},
+          "=0;i<5;i++) $display(\"i = %d\",i);\n",
+          "end\n",
+          "endmodule:foo;\n",
+      },
+  };
   RunLintTestCases<VerilogAnalyzer, InstanceShadowRule>(
       kInstanceShadowingTestCases);
 }
