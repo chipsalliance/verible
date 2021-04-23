f2c8eb1b4d32afe8d9c14a8526dbef7a0b23b762
Jonathan Drolet <jonathan.drolet@gmail.com>
2021-04-23 11:04:20 -0400

    Update verible-transform-interactive-test.sh for Windows

--- common/tools/verible-transform-interactive-test.sh
+++ common/tools/verible-transform-interactive-test.sh
@@ -13,6 +13,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# --- begin runfiles.bash initialization v2 ---
+# Copy-pasted from the Bazel Bash runfiles library v2.
+set -uo pipefail; f=bazel_tools/tools/bash/runfiles/runfiles.bash
+source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "${RUNFILES_MANIFEST_FILE:-/dev/null}" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$0.runfiles/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=; set -e
+# --- end runfiles.bash initialization v2 ---
+
+# Remove exit on failure set by runfiles.bash...
+set +e
+
 # Find input files
 MY_INPUT_FILE="${TEST_TMPDIR}/myinput.txt"
 readonly MY_INPUT_FILE
@@ -28,8 +42,13 @@ readonly MY_EXPECT_FILE
   echo "  verible-patch-tool"
   exit 1
 }
-transform_interactive="$1"
-patch_tool="$2"
+
+# On Windows, runfiles are not symlinked (unless --enable_runfiles is active,
+# which causes other issues on bazel 3.7.0), so use rlocation to find the path
+# to the executable. On Unix, you could simply use $1/$2, but using rlocation
+# works too
+transform_interactive="$(rlocation ${TEST_WORKSPACE}/${1})"
+patch_tool="$(rlocation ${TEST_WORKSPACE}/${2})"
 
 transform_command=("$transform_interactive" --patch-tool="$patch_tool")
 
@@ -75,12 +94,12 @@ EOF
 # Run 'interactive' patcher.
 "${transform_command[@]}" -- grep -v ccc -- "$MY_INPUT_FILE".test*.txt < "$MY_INPUT_FILE".user-input
 
-diff -u "$MY_EXPECT_FILE".test1.txt "$MY_INPUT_FILE".test1.txt || {
+diff -u --strip-trailing-cr "$MY_EXPECT_FILE".test1.txt "$MY_INPUT_FILE".test1.txt || {
   echo "Expected these files to match, but got the above diff."
   exit 1
 }
 
-diff -u "$MY_EXPECT_FILE".test2.txt "$MY_INPUT_FILE".test2.txt || {
+diff -u --strip-trailing-cr "$MY_EXPECT_FILE".test2.txt "$MY_INPUT_FILE".test2.txt || {
   echo "Expected these files to match, but got the above diff."
   exit 1
 }
