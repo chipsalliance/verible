c776fabc43a8382016c2fc3ebab1ce4e02da1980
Jonathan Drolet <jonathan.drolet@gmail.com>
2021-04-25 16:03:49 -0400

    Added wrapper for sh_test to include runfiles lib

--- common/tools/verible-transform-interactive-test.sh
+++ common/tools/verible-transform-interactive-test.sh
@@ -13,20 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# --- begin runfiles.bash initialization v2 ---
-# Copy-pasted from the Bazel Bash runfiles library v2.
-set -uo pipefail; f=bazel_tools/tools/bash/runfiles/runfiles.bash
-source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
-  source "$(grep -sm1 "^$f " "${RUNFILES_MANIFEST_FILE:-/dev/null}" | cut -f2- -d' ')" 2>/dev/null || \
-  source "$0.runfiles/$f" 2>/dev/null || \
-  source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
-  source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
-  { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=; set -e
-# --- end runfiles.bash initialization v2 ---
-
-# Remove exit on failure set by runfiles.bash...
-set +e
-
 # Find input files
 MY_INPUT_FILE="${TEST_TMPDIR}/myinput.txt"
 readonly MY_INPUT_FILE
@@ -42,11 +28,6 @@ readonly MY_EXPECT_FILE
   echo "  verible-patch-tool"
   exit 1
 }
-
-# On Windows, runfiles are not symlinked (unless --enable_runfiles is active,
-# which causes other issues on bazel 3.7.0), so use rlocation to find the path
-# to the executable. On Unix, you could simply use $1/$2, but using rlocation
-# works too
 transform_interactive="$(rlocation ${TEST_WORKSPACE}/${1})"
 patch_tool="$(rlocation ${TEST_WORKSPACE}/${2})"
 
--- verilog/tools/diff/diff_obfuscate_mismatch_test.sh
+++ verilog/tools/diff/diff_obfuscate_mismatch_test.sh
@@ -15,20 +15,6 @@
 
 # Tests verible-verilog-diff --mode=obfuscate
 
-# --- begin runfiles.bash initialization v2 ---
-# Copy-pasted from the Bazel Bash runfiles library v2.
-set -uo pipefail; f=bazel_tools/tools/bash/runfiles/runfiles.bash
-source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
-  source "$(grep -sm1 "^$f " "${RUNFILES_MANIFEST_FILE:-/dev/null}" | cut -f2- -d' ')" 2>/dev/null || \
-  source "$0.runfiles/$f" 2>/dev/null || \
-  source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
-  source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
-  { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=; set -e
-# --- end runfiles.bash initialization v2 ---
-
-# Remove exit on failure set by runfiles.bash...
-set +e
-
 declare -r MY_INPUT_FILE1="${TEST_TMPDIR}/myinput1.txt"
 declare -r MY_INPUT_FILE2="${TEST_TMPDIR}/myinput2.txt"
 
@@ -37,10 +23,6 @@ declare -r MY_INPUT_FILE2="${TEST_TMPDIR}/myinput2.txt"
   echo "Expecting 1 positional argument, verible-verilog-diff path."
   exit 1
 }
-# On Windows, runfiles are not symlinked (unless --enable_runfiles is active,
-# which causes other issues on bazel 3.7.0), so use rlocation to find the path
-# to the executable. On Unix, you could simply use $1, but using rlocation
-# works too
 difftool="$(rlocation ${TEST_WORKSPACE}/${1})"
 
 # different spacing or token length counts as mismatch
