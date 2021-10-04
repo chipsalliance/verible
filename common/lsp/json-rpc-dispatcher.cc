c32e97031c95f7508bdf3bae6bc4a3d98a955ed1
Henner Zeller <h.zeller@acm.org>
2021-10-04 08:46:09 -0700

    Add protocol base support for the language server protocol.
     * JsonRpcDispatcher is implementing the needed functionality
       for JSON-RPC [1] calling notifications and RPC-calls.
     * MessageStreamSplitter is needed in the LSP [2] specifically
       as the transport layer separates each message with a
       header (containing a Content-Length: header) and a body
       (very similar to HTTP).
    
    [1] https://www.jsonrpc.org/specification
    [2] https://microsoft.github.io/language-server-protocol/specification
    
    Signed-off-by: Henner Zeller <h.zeller@acm.org>

---
+++ common/lsp/json-rpc-dispatcher.cc
@@ -0,0 +1,122 @@
+// Copyright 2021 The Verible Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "common/lsp/json-rpc-dispatcher.h"
+
+static constexpr int kParseError = -32700;
+static constexpr int kMethodNotFound = -32601;
+static constexpr int kInternalError = -32603;
+
+namespace verible {
+// Dispatch incoming message, a string view with json data.
+// Call this with the content of exactly one message.
+// If this is an RPC call, it send the response via the WriteFun.
+void JsonRpcDispatcher::DispatchMessage(absl::string_view data) {
+  nlohmann::json request;
+  try {
+    request = nlohmann::json::parse(data);
+  } catch (const std::exception &e) {
+    statistic_counters_[e.what()]++;
+    ++exception_count_;
+    SendReply(CreateError(request, kParseError, e.what()));
+    return;
+  }
+
+  if (request.find("method") == request.end()) {
+    SendReply(
+        CreateError(request, kMethodNotFound, "Method required in request"));
+    statistic_counters_["Request without method"]++;
+    return;
+  }
+  const std::string &method = request["method"];
+
+  // Direct dispatch, later maybe send to thread-pool ?
+  const bool is_notification = (request.find("id") == request.end());
+  bool handled = false;
+  if (is_notification) {
+    handled = CallNotification(request, method);
+  } else {
+    handled = CallRequestHandler(request, method);
+  }
+  statistic_counters_[method + (handled ? "" : " (unhandled)") +
+                      (is_notification ? "  ev" : " RPC")]++;
+}
+
+bool JsonRpcDispatcher::CallNotification(const nlohmann::json &req,
+                                         const std::string &method) {
+  const auto &found = notifications_.find(method);
+  if (found == notifications_.end()) return false;
+  try {
+    found->second(req["params"]);
+    return true;
+  } catch (const std::exception &e) {
+    ++exception_count_;
+    statistic_counters_[method + " : " + e.what()]++;
+  }
+  return false;
+}
+
+bool JsonRpcDispatcher::CallRequestHandler(const nlohmann::json &req,
+                                           const std::string &method) {
+  const auto &found = handlers_.find(method);
+  if (found == handlers_.end()) {
+    SendReply(CreateError(req, kMethodNotFound,
+                          "method '" + method + "' not found."));
+    return false;
+  }
+
+  try {
+    SendReply(MakeResponse(req, found->second(req["params"])));
+    return true;
+  } catch (const std::exception &e) {
+    ++exception_count_;
+    statistic_counters_[method + " : " + e.what()]++;
+    SendReply(CreateError(req, kInternalError, e.what()));
+  }
+  return false;
+}
+
+/*static*/ nlohmann::json JsonRpcDispatcher::CreateError(
+    const nlohmann::json &request, int code, absl::string_view message) {
+  nlohmann::json result = {
+      {"jsonrpc", "2.0"},
+  };
+  result["error"] = {{"code", code}};
+  if (!message.empty()) {
+    result["error"]["message"] = message;
+  }
+
+  if (request.find("id") != request.end()) {
+    result["id"] = request["id"];
+  }
+
+  return result;
+}
+
+/*static*/ nlohmann::json JsonRpcDispatcher::MakeResponse(
+    const nlohmann::json &request, const nlohmann::json &call_result) {
+  nlohmann::json result = {
+      {"jsonrpc", "2.0"},
+  };
+  result["id"] = request["id"];
+  result["result"] = call_result;
+  return result;
+}
+
+void JsonRpcDispatcher::SendReply(const nlohmann::json &response) {
+  std::stringstream out_bytes;
+  out_bytes << response << "\n";
+  write_fun_(out_bytes.str());
+}
+}  // namespace verible
---
+++ common/lsp/message-stream-splitter.h
@@ -0,0 +1,107 @@
+// Copyright 2021 The Verible Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef VERIBLE_COMMON_LSP_MESSAGE_STREAM_SPLITTER_H
+#define VERIBLE_COMMON_LSP_MESSAGE_STREAM_SPLITTER_H
+
+#include <functional>
+#include <memory>
+#include <string>
+
+//
+#include "absl/status/status.h"
+#include "absl/strings/numbers.h"
+#include "absl/strings/str_cat.h"
+#include "absl/strings/string_view.h"
+
+namespace verible {
+// Splits messages that are formatted as header + body coming from some
+// abstracted input stream and calls a handler for each complete message it
+// receives.
+//
+// The MessageStreamSplitter does not read data directly from a source but
+// gets handed a read function to get the data from. This allows to use this
+// in different environments from testing to using it with a filedescriptor
+// event dispatcher (select()).
+// The simplest implementation of the "ReadFun" just wraps a system read() call.
+//
+// The header data MUST contain a Content-Length header.
+class MessageStreamSplitter {
+ public:
+  // A function that reads from some source and writes up to "size" bytes
+  // into the buffer. Returns the number of bytes read.
+  // Blocks until there is content or returns '0' on end-of-file. Values
+  // below zero indicate errors.
+  // Only the amount of bytes available at the time of the call are filled
+  // into the buffer, so the return value can be less than size.
+  // So essentially: this behaves like the standard read() system call.
+  using ReadFun = std::function<int(char *buf, int size)>;
+
+  // Function called with each complete message that has been extracted from
+  // the stream.
+  using MessageProcessFun =
+      std::function<void(absl::string_view header, absl::string_view body)>;
+
+  // Read using an internal buffer of "read_buffer_size", which must be larger
+  // than the largest expected message.
+  explicit MessageStreamSplitter(size_t read_buffer_size)
+      : read_buffer_size_(read_buffer_size),
+        read_buffer_(new char[read_buffer_size]) {}
+  MessageStreamSplitter(const MessageStreamSplitter &) = delete;
+
+  // Set the function that will receive extracted message bodies.
+  void SetMessageProcessor(const MessageProcessFun &message_processor) {
+    message_processor_ = message_processor;
+  }
+
+  // The passed "read_fun()" is called exactly _once_ to get
+  // the next amount of data and calls the message processor for each complete
+  // message found. Partial data received is retained to be re-considered on
+  // the next call to PullFrom().
+  //
+  // Within the context of this method, the message processor might be
+  // called zero to multiple times depending on how much data arrives from
+  // the read.
+  //
+  // Note: The once-call behaviour allows to hook this into some file-descriptor
+  // event dispatcher (e.g using select()).
+  //
+  // Returns with an ok status until EOF or some error occurs.
+  // Code
+  //  - kUnavailable     : regular EOF, no data pending. A 'good' non-ok status.
+  //  - kDataloss        : got EOF, but still incomplete data pending.
+  //  - kInvalidargument : stream corrupted, couldn't read header.
+  absl::Status PullFrom(const ReadFun &read_fun);
+
+  // -- Statistical data
+
+  uint64_t StatLargestBodySeen() const { return stats_largest_body_; }
+  uint64_t StatTotalBytesRead() const { return stats_total_bytes_read_; }
+
+ private:
+  static int ParseHeaderGetBodyOffset(absl::string_view data, int *body_size);
+  absl::Status ProcessContainedMessages(absl::string_view *data);
+  absl::Status ReadInput(const ReadFun &read_fun);
+
+  const size_t read_buffer_size_;
+  std::unique_ptr<char[]> read_buffer_;
+
+  MessageProcessFun message_processor_;
+
+  size_t stats_largest_body_ = 0;
+  uint64_t stats_total_bytes_read_ = 0;
+  absl::string_view pending_data_;
+};
+}  // namespace verible
+#endif  // VERIBLE_COMMON_LSP_MESSAGE_STREAM_SPLITTER_H
