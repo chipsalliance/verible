ebd1e492759f794c3f6903295ac51ddf59ffb24e
karimtera <karim.tera1@gmail.com>
2022-07-14 01:18:28 +0200

    applying clang-format

--- verilog/analysis/flow_tree.h
+++ verilog/analysis/flow_tree.h
@@ -15,10 +15,10 @@
 #ifndef VERIBLE_VERILOG_FLOW_TREE_H_
 #define VERIBLE_VERILOG_FLOW_TREE_H_
 
-
-#include <vector>
 #include <map>
 #include <string>
+#include <vector>
+
 #include "absl/status/status.h"
 #include "common/lexer/token_stream_adapter.h"
 #include "verilog/parser/verilog_token_enum.h"
@@ -27,19 +27,21 @@ namespace verilog {
 
 class FlowTree {
  public:
-  explicit FlowTree(verible::TokenSequence source_sequence): source_sequence_(std::move(source_sequence)){};
+  explicit FlowTree(verible::TokenSequence source_sequence)
+      : source_sequence_(std::move(source_sequence)){};
 
   absl::Status GenerateControlFlowTree();
   absl::Status DepthFirstSearch(int index);
   std::vector<verible::TokenSequence> variants_;
+
  private:
   std::vector<int> ifs_;
-  std::map<int,std::vector<int>> elses_; 
-  std::map<int,std::vector<int>> edges_; 
+  std::map<int, std::vector<int>> elses_;
+  std::map<int, std::vector<int>> edges_;
   verible::TokenSequence source_sequence_;
   verible::TokenSequence current_sequence_;
 };
 
-} // namespace verilog
+}  // namespace verilog
 
-#endif // VERIBLE_VERILOG_FLOW_TREE_H_
+#endif  // VERIBLE_VERILOG_FLOW_TREE_H_
