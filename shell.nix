8eedf4914665d0845b3beafcfb8fdffcacf12281
Henner Zeller <h.zeller@acm.org>
2022-09-08 15:47:19 -0700

    No need to pin an old nix version, just use whatever user is using.
    Signed-off-by: Henner Zeller <h.zeller@acm.org>

--- shell.nix
+++ shell.nix
@@ -2,16 +2,7 @@
 # If you have nix installed, you may simply run `nix-shell`
 # in this repo, and have all dependencies ready in the new shell.
 
-{pkgs ? import (builtins.fetchTarball {
-  # Descriptive name to make the store path easier to identify
-  name = "nixos-2021-11";
-  # Commit hash
-  url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/21.11.tar.gz";
-  # Hash obtained using `nix-prefetch-url --unpack <url>`
-  sha256 = "162dywda2dvfj1248afxc45kcrg83appjd0nmdb541hl7rnncf02";
-}) {}}:
-
-
+{ pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
   buildInputs = with pkgs;
     [
@@ -23,6 +14,8 @@ pkgs.mkShell {
       jdk11
       lcov
       python3
-    ];
 
+      # Ease development
+      clang-tools_11  # clang-format
+    ];
 }
