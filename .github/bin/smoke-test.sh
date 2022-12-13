3d05e05135e3aca74be4c5780f6b188ad35c2a0d
Henner Zeller <hzeller@google.com>
2022-12-13 04:39:08 -0800

    Update smoketest expectations.
    Signed-off-by: Henner Zeller <hzeller@google.com>

--- .github/bin/smoke-test.sh
+++ .github/bin/smoke-test.sh
@@ -137,13 +137,13 @@ ExpectedFailCount[project:ibex]=192
 ExpectedFailCount[syntax:opentitan]=36
 ExpectedFailCount[lint:opentitan]=36
 ExpectedFailCount[formatter:opentitan]=1
-ExpectedFailCount[project:opentitan]=725
+ExpectedFailCount[project:opentitan]=727
 
 ExpectedFailCount[project:Cores-SweRV]=21
 
 ExpectedFailCount[syntax:cva6]=4
 ExpectedFailCount[lint:cva6]=4
-ExpectedFailCount[project:cva6]=38
+ExpectedFailCount[project:cva6]=70
 
 ExpectedFailCount[syntax:uvm]=1
 ExpectedFailCount[lint:uvm]=1
@@ -185,9 +185,9 @@ ExpectedFailCount[project:rsd]=43
 ExpectedFailCount[formatter:scr1]=2
 ExpectedFailCount[project:scr1]=45
 
-ExpectedFailCount[syntax:basejump_stl]=460
-ExpectedFailCount[lint:basejump_stl]=460
-ExpectedFailCount[project:basejump_stl]=566
+ExpectedFailCount[syntax:basejump_stl]=461
+ExpectedFailCount[lint:basejump_stl]=461
+ExpectedFailCount[project:basejump_stl]=567
 
 # Ideally, we expect all tools to process all files with a zero exit code.
 # However, that is not always the case, so we document the current
