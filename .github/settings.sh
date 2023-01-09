58131249090ee5e0c4c9794b25453c007d631af6
Ivan Krišto <ikr@google.com>
2023-01-09 16:02:02 +0100

    Up the bazel version from 4.0.0 to 5.4.0 in the attempt to fix the CI build

--- .github/settings.sh
+++ .github/settings.sh
@@ -32,7 +32,7 @@ export BAZEL_CXXOPTS="-std=c++17"
 export BAZEL_OPTS="-c opt --show_progress_rate_limit=10.0"
 
 # Used to fetch the BAZEL version where needed.
-export BAZEL_VERSION=4.0.0
+export BAZEL_VERSION=5.4.0
 
 # TODO(b/171989992): revert to using release version after upgrading system
 #   libraries/image (need: GLIBCXX_3.4.26, CXXABI_1.3.11, GLIBC_2.29).
