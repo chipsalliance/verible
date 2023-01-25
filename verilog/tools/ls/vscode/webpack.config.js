6ced49c887d7eeaa633f77e0533ac017d3869990
Henner Zeller <hzeller@google.com>
2023-01-25 15:37:48 -0800

    Use spaces, not tabs.
    Signed-off-by: Henner Zeller <hzeller@google.com>

--- verilog/tools/ls/vscode/webpack.config.js
+++ verilog/tools/ls/vscode/webpack.config.js
@@ -10,7 +10,7 @@ const path = require('path');
 /** @type WebpackConfig */
 const extensionConfig = {
   target: 'node', // vscode extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
-	mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
+  mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
 
   entry: './src/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
   output: {
