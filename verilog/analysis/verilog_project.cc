1d212d3d040755d01d6046bc6f3b1019e6d430d2
Henner Zeller <h.zeller@acm.org>
2024-01-29 10:32:13 -0800

    Fix some missing includes/dependencies.

--- verilog/analysis/verilog_project.cc
+++ verilog/analysis/verilog_project.cc
@@ -22,6 +22,7 @@
 #include <vector>
 
 #include "absl/status/status.h"
+#include "absl/status/statusor.h"
 #include "absl/strings/str_cat.h"
 #include "absl/strings/str_join.h"
 #include "absl/strings/string_view.h"
