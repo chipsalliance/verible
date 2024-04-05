ccd529d693f458079fe58cbbb025c7e2024b1869
Steven Conway <stevenconwayaus@gmail.com>
2024-04-05 23:16:59 +1100

    Pull request https://github.com/chipsalliance/verible/pull/1336 seems to have stalled. Copied the code from https://github.com/suzizecat/verible/commit/0b087d72cfa58e9e8c34a164d4abaeda9880ca7d and rebased it to the head. Updated the tests and formatted.

---
+++ verilog/analysis/checkers/explicit_begin_rule.h
@@ -0,0 +1,67 @@
+// Copyright 2017-2020 The Verible Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef VERIBLE_VERILOG_ANALYSIS_CHECKERS_EXPLICIT_BEGIN_RULE_H_
+#define VERIBLE_VERILOG_ANALYSIS_CHECKERS_EXPLICIT_BEGIN_RULE_H_
+
+#include <set>
+#include <stack>
+#include <string>
+
+#include "common/analysis/lint_rule_status.h"
+#include "common/analysis/token_stream_lint_rule.h"
+#include "common/text/token_info.h"
+#include "verilog/analysis/descriptions.h"
+
+namespace verilog {
+namespace analysis {
+
+// Detects whether if and for loop statements use verilog block statements (begin/end)
+class ExplicitBeginRule : public verible::TokenStreamLintRule {
+ public:
+  using rule_type = verible::TokenStreamLintRule;
+
+  static const LintRuleDescriptor &GetDescriptor();
+
+  ExplicitBeginRule() : state_(State::kNormal), condition_expr_level_(0) {}
+
+  void HandleToken(const verible::TokenInfo &token) final;
+
+  verible::LintRuleStatus Report() const final;
+
+ private:
+  // States of the internal token-based analysis.
+  enum class State { kNormal, kInCondition, kInElse, kExpectBegin };
+
+  // Internal lexical analysis state.
+  State state_;
+
+  // Level of nested parenthesis when analizing conditional expressions.
+  int condition_expr_level_;
+
+  // Token information for the last seen block opening (for/if/else).
+  verible::TokenInfo last_condition_start_ = verible::TokenInfo::EOFToken();
+  verible::TokenInfo end_of_condition_statement_ =
+      verible::TokenInfo::EOFToken();
+
+  // Collection of found violations.
+  std::set<verible::LintViolation> violations_;
+
+  void trigger_violation_();
+};
+
+}  // namespace analysis
+}  // namespace verilog
+
+#endif  // VERIBLE_VERILOG_ANALYSIS_CHECKERS_ENDIF_COMMENT_RULE_H_
---
+++ verilog/analysis/checkers/explicit_begin_rule_test.cc
@@ -0,0 +1,93 @@
+// Copyright 2017-2020 The Verible Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "verilog/analysis/checkers/explicit_begin_rule.h"
+
+#include <initializer_list>
+
+#include "common/analysis/linter_test_utils.h"
+#include "common/analysis/token_stream_linter_test_utils.h"
+#include "common/text/symbol.h"
+#include "gtest/gtest.h"
+#include "verilog/analysis/verilog_analyzer.h"
+#include "verilog/parser/verilog_token_enum.h"
+
+namespace verilog {
+namespace analysis {
+namespace {
+
+using verible::LintTestCase;
+using verible::RunApplyFixCases;
+using verible::RunLintTestCases;
+
+// Tests that space-only text passes.
+TEST(ExplicitBeginRuleTest, AcceptsBlank) {
+  const std::initializer_list<LintTestCase> kTestCases = {
+      {""},
+      {" "},
+      {"\n"},
+      {" \n\n"},
+  };
+  RunLintTestCases<VerilogAnalyzer, ExplicitBeginRule>(kTestCases);
+}
+
+// Tests that properly matched if/begin passes.
+TEST(ExplicitBeginRuleTest, AcceptsBlocksWithBegin) {
+  const std::initializer_list<LintTestCase> kTestCases = {
+      {"if (FOO) begin"},
+      {"if (FOO)\n begin"},
+      {"if (FOO) //Comment\n begin"},
+      {"else begin \n FOO"},
+      {"else \nbegin \n FOO"},
+      {"else //Comment\n begin \n FOO"},
+      {"else \n //Comment\n begin \n FOO"},
+      {"else if (FOO) begin"},
+      {"else if (FOO)\n begin"},
+      {"else if (FOO) //Comment\n begin"},
+      {"else if (FOO)\n //Comment\n begin"},
+      {"for(i = 0; i < N; i++) begin"},
+      {"for(i = 0; i < N; i++)\nbegin"},
+      {"for(i = 0; i < N; i++) // Comment\n begin"},
+      {"for(i = 0; i < N; i++)\n // Comment\nbegin"},
+      // {"forever begin"},     // Not supported
+  };
+  RunLintTestCases<VerilogAnalyzer, ExplicitBeginRule>(kTestCases);
+}
+
+// Tests that unmatched block/begin fails is detected.
+TEST(ExplicitBeginRuleTest, RejectBlocksWithoutBegin) {
+  const std::initializer_list<LintTestCase> kTestCases = {
+      {{TK_if, "if"}, " (FOO) BAR"},
+      {{TK_if, "if"}, " (FOO)\n BAR"},
+      {{TK_if, "if"}, " (FOO) //Comment\n BAR"},
+      {{TK_else, "else"}, " \n FOO"},
+      {{TK_else, "else"}, " \n \n FOO"},
+      {{TK_else, "else"}, " //Comment\n  FOO"},
+      {{TK_else, "else"}, " \n //Comment\n FOO"},
+      {"else ", {TK_if, "if"}, " (FOO) BAR"},
+      {"else ", {TK_if, "if"}, " (FOO)\n BAR"},
+      {"else ", {TK_if, "if"}, " (FOO) //Comment\n BAR"},
+      {"else ", {TK_if, "if"}, " (FOO)\n //Comment\n BAR"},
+      {{TK_for, "for"}, "(i = 0; i < N; i++) a <= 1'b1;"},
+      {{TK_for, "for"}, "(i = 0; i < N; i++)\n a <= 1'b1;"},
+      {{TK_for, "for"}, "(i = 0; i < N; i++) // Comment \n a <= 1'b1;"},
+      {{TK_for, "for"}, "(i = 0; i < N; i++)\n // Comment\n a <= 1'b1;"},
+      // {{TK_forever, "forever"}, "a <= 1'b1;"},   // Not supported
+  };
+  RunLintTestCases<VerilogAnalyzer, ExplicitBeginRule>(kTestCases);
+}
+
+}  // namespace
+}  // namespace analysis
+}  // namespace verilog
