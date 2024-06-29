394c9ef2ee2dff1bb74a38c55c5523ae90a89bdc
Tim 'mithro' Ansell <me@mith.ro>
2024-06-29 10:06:28 -0700

    Adding minimal `conf.py` file
    Adding a minimal `conf.py` file to the `docs` directory based on the last passing build @ https://readthedocs.org/projects/verible/builds/21293135/
    
    Still unclear if rtd will still add the remaining bits needed.

---
+++ doc/conf.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import division, print_function, unicode_literals
+
+from datetime import datetime
+
+from recommonmark.parser import CommonMarkParser
+
+extensions = []
+templates_path = ['templates', '_templates', '.templates']
+source_suffix = ['.rst', '.md']
+source_parsers = {
+            '.md': CommonMarkParser,
+        }
+master_doc = 'index'
+project = u'verible'
+copyright = str(datetime.now().year)
+version = 'latest'
+release = 'latest'
+exclude_patterns = ['_build']
+pygments_style = 'sphinx'
+htmlhelp_basename = 'verible'
+html_theme = 'sphinx_rtd_theme'
+file_insertion_enabled = False
+latex_documents = [
+  ('index', 'verible.tex', u'verible Documentation',
+   u'', 'manual'),
+]
