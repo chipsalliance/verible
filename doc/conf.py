485b839b7ad01aa141db70c7a3c323142c3adbba
Tim 'mithro' Ansell <me@mith.ro>
2024-06-29 11:02:22 -0700

    Switch docs to use `myst_parser` for Markdown parsing
    Seeing if this change fixes the documentation output.

--- doc/conf.py
+++ doc/conf.py
@@ -1,17 +1,14 @@
 # -*- coding: utf-8 -*-
 
-from __future__ import division, print_function, unicode_literals
-
 from datetime import datetime
 
-from recommonmark.parser import CommonMarkParser
-
-extensions = []
+extensions = ['myst_parser']
 templates_path = ['templates', '_templates', '.templates']
 source_suffix = ['.rst', '.md']
 source_parsers = {
-            '.md': CommonMarkParser,
-        }
+      '.rst': 'restructuredtext',
+      '.md': 'markdown',
+}
 master_doc = 'index'
 project = u'verible'
 copyright = str(datetime.now().year)
@@ -23,6 +20,5 @@ htmlhelp_basename = 'verible'
 html_theme = 'sphinx_rtd_theme'
 file_insertion_enabled = False
 latex_documents = [
-  ('index', 'verible.tex', u'verible Documentation',
-   u'', 'manual'),
+  ('index', 'verible.tex', u'verible Documentation', u'', 'manual'),
 ]
