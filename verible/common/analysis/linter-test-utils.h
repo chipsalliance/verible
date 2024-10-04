a49439ea425910c7c9e518332490b1bf97ce9576
Ignacio Encinas <ignacio@iencinas.com>
2024-10-04 22:27:59 +0200

    linter_test_utils: implement multi-violation AutoFix tests
    Allow AutoFix test cases to specify the violation for which the AutoFix
    test is intended.
    
    Keep backwards compatibility by defaulting the new field to 0, given
    that existing tests assume there is only violation per test case.

--- verible/common/analysis/linter-test-utils.h
+++ verible/common/analysis/linter-test-utils.h
@@ -18,6 +18,7 @@
 #include <functional>
 #include <initializer_list>
 #include <iosfwd>
+#include <iterator>
 #include <memory>
 #include <set>
 #include <sstream>
@@ -117,6 +118,7 @@ struct AutoFixInOut {
   absl::string_view code;
   absl::string_view expected_output;
   int fix_alternative = 0;  // Some rules provide alternative fixes
+  int violation_number = 0;
 };
 
 // Tests that LintTestCase test has expected violations under make_rule
@@ -134,10 +136,11 @@ void RunLintAutoFixCase(const AutoFixInOut &test,
   const LintRuleStatus rule_status = lint_runner.Run(analyzer.Data(), filename);
   const auto &violations(rule_status.violations);
 
-  CHECK_EQ(violations.size(), 1) << "TODO: apply multi-violation fixes";
-  CHECK_GT(violations.begin()->autofixes.size(), test.fix_alternative);
-  const verible::AutoFix &fix =
-      rule_status.violations.begin()->autofixes[test.fix_alternative];
+  CHECK_GT(violations.size(), test.violation_number);
+  const LintViolation &violation =
+      *std::next(violations.begin(), test.violation_number);
+  CHECK_GT(violation.autofixes.size(), test.fix_alternative);
+  const verible::AutoFix &fix = violation.autofixes[test.fix_alternative];
   std::string fix_out = fix.Apply(analyzer.Data().Contents());
 
   EXPECT_EQ(test.expected_output, fix_out) << "For input " << test.code;
