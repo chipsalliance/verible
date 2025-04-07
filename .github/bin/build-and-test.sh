f02dbe286910c49c6fca93ce29a6d666049646e4
Henner Zeller <h.zeller@acm.org>
2025-04-07 14:43:10 -0700

    Simplify settings of warnings.
    Enable warnings in .bazelrc (and disable the ones that are
    benign). Only in CI, we make it -Werror.

--- .github/bin/build-and-test.sh
+++ .github/bin/build-and-test.sh
@@ -40,52 +40,14 @@ fi
 # as possible to compile without exceptions.
 BAZEL_OPTS="${BAZEL_OPTS} --cxxopt=-fno-exceptions"
 
+# Avoid expensive language feature. Maybe should consider to put into .bazelrc
+BAZEL_OPTS="${BAZEL_OPTS} --cxxopt=-fno-rtti"
+
 # Turn warnings to 11. And fail compliation if we encounter one.
 BAZEL_OPTS="${BAZEL_OPTS} --cxxopt=-Werror"  # Always want bail on warning
-BAZEL_OPTS="${BAZEL_OPTS} --cxxopt=-W --cxxopt=-Wall --cxxopt=-Wextra"
 
 # The following warning only reports with clang++; it is ignored by gcc
 BAZEL_OPTS="${BAZEL_OPTS} --cxxopt=-Wunreachable-code"
-
-# -- now disable some of the warnings that happen, so that the compile finishes.
-
-# Status-quo of warnings happening in our code-base. These are benign.
-BAZEL_OPTS="${BAZEL_OPTS} --cxxopt=-Wno-unused-parameter"
-BAZEL_OPTS="${BAZEL_OPTS} --cxxopt=-Wno-missing-field-initializers"
-
-# Warnings in our code-base, that we might consider removing.
-BAZEL_OPTS="${BAZEL_OPTS} --cxxopt=-Wno-redundant-move"
-
-# Newer bisons produce an unused label in generated code
-BAZEL_OPTS="${BAZEL_OPTS} --cxxopt=-Wno-unused-label"
-
-# Compiler evaluates  sizeof...(args) and complains about zero
-BAZEL_OPTS="${BAZEL_OPTS} --cxxopt=-Wno-type-limits"
-
-# Warnings that come from other external parts that we compile.
-# Ideally, we would separate them out to ignore only there, while we keep
-# tight warnings on for 'our' code-base.
-# TODO(hzeller): Remove after
-#            https://github.com/chipsalliance/verible/issues/747 is figured out
-if [[ "${CXX}" == clang* ]]; then
-  # -- only recognized by clang
-  # Don't rely on implicit template type deduction
-  BAZEL_OPTS="${BAZEL_OPTS} --cxxopt=-Wctad-maybe-unsupported"
-
-  # Also warn about string conversion issues.
-  BAZEL_OPTS="${BAZEL_OPTS} --cxxopt=-Wstring-conversion"
-
-  BAZEL_OPTS="${BAZEL_OPTS} --cxxopt=-Wno-unused-function"  # utf8_range dependencyo
-
-  BAZEL_TEST_OPTS="--copt -D_LIBCPP_ENABLE_DEBUG_MODE"
-else  # gcc
-  # disabled for now https://github.com/chipsalliance/verible/issues/1056
-  #BAZEL_TEST_OPTS="--copt -D_GLIBCXX_DEBUG"
-  PLACEHOLDER_ASSIGNMENT_SO_THAT_ELSE_BRANCH_DOES_NOT_CREATE_SYNTAX_ERROR=1
-fi
-
-# Protobuf triggers a maybe-uninitialized warning.
-BAZEL_OPTS="${BAZEL_OPTS} --cxxopt=-Wno-uninitialized"
 
 # If parameter given and the MODE allows choosing, we build the target
 # as provided, otherwise all. This allows manual invocation of interesting
@@ -95,10 +57,6 @@ CHOSEN_TARGETS=${@:-//...}
 case "$MODE" in
   test|test-clang)
     bazel test --keep_going --cache_test_results=no --test_output=errors ${BAZEL_OPTS} ${BAZEL_TEST_OPTS} ${CHOSEN_TARGETS}
-    ;;
-
-  test-nortti)
-    bazel test --keep_going --cache_test_results=no --test_output=errors ${BAZEL_OPTS} --cxxopt=-fno-rtti ${CHOSEN_TARGETS}
     ;;
 
   asan|asan-clang)
