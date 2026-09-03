bd63b1b40ca4002ccfe53ce7e04705311910a789
Matteo Lupi <matteo.lupi@cern.ch>
2026-09-03 14:45:44 +0200

    ML test analog construct alongside analog as identifier

--- verible/verilog/parser/verilog-parser_test.cc
+++ verible/verilog/parser/verilog-parser_test.cc
@@ -4749,6 +4749,18 @@ static constexpr ParserTestCaseArray kModuleMemberTests = {
     "    `MACRO();\n"  // macro as item
     "  }\n"
     "endgroup",
+    // Verilog-AMS analog_construct, which starts with the same "analog" that is
+    // also accepted as a plain identifier.
+    "module mymodule;\n"
+    "  analog V(a, b) <+ 1;\n"
+    "endmodule",
+    "module mymodule;\n"
+    "  analog I(x) <+ y + 1;\n"
+    "endmodule",
+    "module mymodule;\n"
+    "  reg analog;\n"  // analog as identifier, next to the construct
+    "  analog V(a, b) <+ analog;\n"
+    "endmodule",
 };
 
 static constexpr ParserTestCaseArray kClassMemberTests = {
